f61bf51fe530500371d865c4b173d7d309aa6468
AlanTellezS <43708959+AlanTellezS@users.noreply.github.com>
2020-07-28 19:52:57 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,7 +114,7 @@ async def on_raw_reaction_add(payload):
 
 @client.event
 async def on_raw_reaction_remove(payload):
-    print(emoji)
+    print(payload.emoji.name)
     for listener in listeners:
         if listener['message_id'] == payload.message_id:
             for emoji in listener['emojis_allowed']:
